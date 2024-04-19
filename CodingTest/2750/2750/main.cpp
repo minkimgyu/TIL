a3e71413cb95760f30e049ce8cc2a77d140218b4
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-04-20 02:18:56 +0900

    Update problem

---
+++ CodingTest/2750/2750/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if(i == n - 1) cout << arr[i];
+		else cout << arr[i] << '\n';
+		
+	}
+
+	return 0;
+}
