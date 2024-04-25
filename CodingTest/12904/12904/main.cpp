02fe14fa0b55c983b2b86eb4d4ad03f114152961
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-04-26 01:52:48 +0900

    Update problems

---
+++ CodingTest/12904/12904/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	const int size1 = 1000;
+	const int size2 = 1001;
+
+	string s;
+	string t;
+
+	getline(cin, s);
+	getline(cin, t);
+
+	while (t.size() >= s.size())
+	{
+		char item = t.back();
+		if (item == 'A')
+		{
+			t.pop_back();
+		}
+		else if (item == 'B')
+		{
+			t.pop_back();
+
+			int size = t.size();
+			for (int i = 0; i < size / 2; i++)
+			{
+				char back = t[size - i - 1];
+				t[size - i - 1] = t[i];
+				t[i] = back;
+			}
+		}
+
+		bool isSame = true;
+		int size = t.size();
+		if (s.size() == size)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				if (s[i] != t[i]) isSame = false;
+			}
+
+			if (isSame == true)
+			{
+				cout << 1;
+				return 0;
+			}
+		}
+		else if (s.size() > size)
+		{
+			cout << 0;
+			return 0;
+		}
+	}
+
+	cout << 0;
+	return 0;
+}
---
+++ CodingTest/2644/2644/main.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+	vector<int> _closeNodeIndexes;
+	bool _visit = false;
+	int icon = 0;
+	int depth = 0;
+};
+
+class Queue
+{
+public:
+	void Push(Node* node)
+	{
+		_list.push_back(node);
+	}
+
+	void Pop()
+	{
+		_list.pop_front();
+	}
+
+	Node* Front()
+	{
+		return _list.front();
+	}
+
+	Node* Back()
+	{
+		return _list.back();
+	}
+
+	int Size()
+	{
+		return _list.size();
+	}
+
+	bool IsEmpty()
+	{
+		return Size() == 0;
+	}
+
+private:
+	list<Node*> _list;
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	Node* nodes = new Node[n];
+	for (int i = 0; i < n; i++)
+	{
+		nodes[i].icon = i + 1;
+	}
+
+	int start, end;
+	cin >> start >> end;
+
+	int link;
+	cin >> link;
+	for (int i = 0; i < link; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		nodes[a - 1]._closeNodeIndexes.push_back(b - 1);
+		nodes[b - 1]._closeNodeIndexes.push_back(a - 1);
+	}
+
+	int cnt;
+
+	Queue queue;
+	
+	nodes[start - 1]._visit = true;
+	queue.Push(&nodes[start - 1]);
+
+	while (queue.IsEmpty() == false)
+	{
+		Node* node = queue.Front();
+		queue.Pop();
+
+		int depth = node->depth;
+		if (node->icon == end)
+		{
+			cout << depth;
+			return 0;
+		}
+
+		depth++; // 여기서 depth 증가
+
+		int size = node->_closeNodeIndexes.size();
+		for (int i = 0; i < size; i++)
+		{
+			int index = node->_closeNodeIndexes[i];
+			if (nodes[index]._visit == true) continue;
+			
+			nodes[index].depth = depth; // depth 키워주기
+			nodes[index]._visit = true;
+			queue.Push(&nodes[index]);
+		}
+	}
+
+	cout << -1;
+
+	return 0;
+}
