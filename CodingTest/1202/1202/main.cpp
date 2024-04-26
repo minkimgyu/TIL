e1164d4edbf76eaa7922746722288815a5ec5988
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-04-27 01:00:35 +0900

    Update problems

---
+++ CodingTest/1202/1202/main.cpp
@@ -0,0 +1,176 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Diamond
+{
+public:
+	int _weight;
+	int _price;
+};
+
+struct Bag
+{
+public:
+	int _emptySpace;
+	bool _isFull = false;
+};
+
+class PriorityQueue
+{
+public:
+	void Insert(Diamond* diamond)
+	{
+		_vec.push_back(diamond);
+		int size = Size();
+
+		PercolateUp(size - 1);
+	}
+
+	Diamond* GetMax()
+	{
+		return _vec[0];
+	}
+
+	void DeleteMax()
+	{
+		int size = Size();
+		_vec[0] = _vec[size - 1];
+		_vec.pop_back();
+
+		PercolateDown(0);
+	}
+
+	int Size()
+	{
+		return _vec.size();
+	}
+
+	bool IsEmpty()
+	{
+		return Size() == 0;
+	}
+
+private:
+	int GetParentIndex(int index)
+	{
+		return (int)((index - 1) / 2);
+	}
+
+	int GetLeftChildIndex(int index)
+	{
+		return 2 * index + 1;
+	}
+
+	int GetRightChildIndex(int index)
+	{
+		return 2 * index + 2;
+	}
+
+	void Swap(int aIndex, int bIndex)
+	{
+		Diamond* a = _vec[aIndex];
+		_vec[aIndex] = _vec[bIndex];
+		_vec[bIndex] = a;
+	}
+
+	void PercolateDown(int index)
+	{
+		int currentIndex = index;
+		int childIndex = GetLeftChildIndex(index);
+		int rightChildIndex = GetRightChildIndex(index);
+
+		int size = Size();
+		if (rightChildIndex < size && _vec[childIndex]->_price < _vec[rightChildIndex]->_price) childIndex = rightChildIndex;
+
+		while (childIndex < size && _vec[currentIndex]->_price < _vec[childIndex]->_price)
+		{
+			Swap(childIndex, currentIndex);
+
+			currentIndex = childIndex;
+			childIndex = GetLeftChildIndex(currentIndex);
+			rightChildIndex = GetRightChildIndex(currentIndex);
+
+			if (rightChildIndex < size && _vec[childIndex]->_price < _vec[rightChildIndex]->_price) childIndex = rightChildIndex;
+		}
+	}
+
+	void PercolateUp(int index)
+	{
+		int currentIndex = index;
+		int parentIndex = GetParentIndex(currentIndex);
+
+		while (parentIndex >= 0 && _vec[parentIndex]->_price < _vec[currentIndex]->_price)
+		{
+			Swap(parentIndex, currentIndex);
+			currentIndex = parentIndex;
+			parentIndex = GetParentIndex(currentIndex);
+		}
+	}
+
+
+private:
+	vector<Diamond*> _vec;
+};
+
+bool compare1(Diamond a, Diamond b)
+{
+	return a._weight < b._weight;
+}
+
+bool compare2(Bag a, Bag b)
+{
+	return a._emptySpace < b._emptySpace;
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+
+	Diamond* diamonds = new Diamond[n];
+	Bag* bags = new Bag[k];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> diamonds[i]._weight >> diamonds[i]._price;
+	}
+
+	for (int i = 0; i < k; i++)
+	{
+		cin >> bags[i]._emptySpace;
+	}
+
+	sort(diamonds, diamonds + n, compare1);
+	sort(bags, bags + k, compare2);
+
+	long long int sum = 0;
+	PriorityQueue pQ;
+
+	int index = 0;
+	for (int i = 0; i < k; i++)
+	{
+		// 이렇게 하면 시간 복잡도가 n * k가 아니라 n + k가 된다.
+		while (index < n && bags[i]._emptySpace >= diamonds[index]._weight)
+		{
+			pQ.Insert(&diamonds[index]);
+			index++;
+		}
+
+		if (pQ.IsEmpty() == true) continue;
+
+		Diamond* dia = pQ.GetMax();
+		pQ.DeleteMax();
+		sum += dia->_price;
+	}
+
+	cout << sum;
+
+	return 0;
+}
---
+++ CodingTest/1926/main.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+struct Vector2
+{
+public:
+	int _x, _y;
+};
+
+class Queue
+{
+public:
+	void Push(Vector2 vec)
+	{
+		_list.push_back(vec);
+	}
+
+	void Pop()
+	{
+		_list.pop_front();
+	}
+
+	Vector2 Front()
+	{
+		return _list.front();
+	}
+
+	Vector2 Back()
+	{
+		return _list.back();
+	}
+
+	int Size()
+	{
+		return _list.size();
+	}
+
+	bool IsEmpty()
+	{
+		return Size() == 0;
+	}
+
+private:
+	list<Vector2> _list;
+};
+
+const int maxSize = 500;
+int map[maxSize][maxSize];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int r, c;
+	cin >> r >> c;
+
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
+
+	int visit = 2;
+
+	int cnt = 0;
+	int maxArtSize = 0;
+
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			Vector2 start;
+			start._y = i;
+			start._x = j;
+
+			int artSize = 0;
+
+			if (map[start._y][start._x] == 0 || map[start._y][start._x] == visit) continue;
+			map[start._y][start._x] = visit;
+
+			artSize++;
+			if (artSize > maxArtSize) maxArtSize = artSize;
+
+			cnt++;
+
+			Queue queue;
+			queue.Push(start);
+
+			while (!queue.IsEmpty())
+			{
+				Vector2 vec = queue.Front();
+				queue.Pop();
+
+				vector<Vector2> closePos;
+				Vector2 left = vec;
+				left._x--;
+				if (left._x >= 0) closePos.push_back(left);
+
+				Vector2 right = vec;
+				right._x++;
+				if (right._x < c) closePos.push_back(right);
+
+				Vector2 up = vec;
+				up._y--;
+				if (up._y >= 0) closePos.push_back(up);
+
+				Vector2 down = vec;
+				down._y++;
+				if (down._y < r) closePos.push_back(down);
+
+				int closeNodeSize = closePos.size();
+				for (int z = 0; z < closeNodeSize; z++)
+				{
+					if (map[closePos[z]._y][closePos[z]._x] == 0 ||
+						map[closePos[z]._y][closePos[z]._x] == visit) continue;
+
+					map[closePos[z]._y][closePos[z]._x] = visit;
+					queue.Push(closePos[z]);
+
+					artSize++;
+					if (artSize > maxArtSize) maxArtSize = artSize;
+				}
+			}
+
+		}
+	}
+
+	cout << cnt << '\n';
+	cout << maxArtSize;
+
+	return 0;
+}
