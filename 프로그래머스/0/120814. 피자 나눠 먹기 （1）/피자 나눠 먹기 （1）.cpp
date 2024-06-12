519dbef337aec92ee929864f2908e31fd7326f1a
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-06-13 03:46:46 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    answer = n / 7;
+    if(n % 7 > 0) answer++;
+    
+    return answer;
+}
