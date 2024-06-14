3e62dc883f6519df71f840d5af5f3b831b5c6e4b
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-06-14 23:46:48 +0900

    Update problem

---
+++ CodingTest/1788/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+const int mod = 1000000000;
+const int dpSize = 1000000;
+int dp[dpSize];
+
+int fibo(int n)
+{
+	int mulit = 1;
+
+	if (n < 0)
+	{
+		n *= -1;
+
+		if (n % 2 == 0)
+		{
+			mulit *= -1;
+		}
+	}
+
+	dp[0] = 0;
+	dp[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+	}
+
+	return dp[n] * mulit;
+}
+
+// 단순히 재귀로 문제를 해결하기에 너무 많은 시간이 소요된다.
+// 따라서 dp로 접근
+// 음수 영역까지 dp를 만드는 것보단 양수 영역 dp에서 계산해주는 것이 맞음
+// 여기서 n % 2 == 0를 고려해서 적용해준다.
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int tmp = fibo(n);
+	if (tmp < 0)
+	{
+		cout << -1 << '\n';
+		cout << tmp * -1;
+	}
+	else if(tmp > 0)
+	{
+		cout << 1 << '\n';
+		cout << tmp;
+	}
+	else
+	{
+		cout << 0 << '\n';
+		cout << tmp;
+	}
+
+	return 0;
+}
---
+++ CodingTest/7562/main.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+const int moveCnt = 8;
+pair<int, int> movePos[moveCnt] 
+= { {2, 1},  {-2, 1}, {2, -1}, {-2, -1},
+	{1, 2}, {1, -2}, {-1, -2}, {-1, 2} };
+
+template <typename T>
+class Queue
+{
+public:
+	void Push(T item)
+	{
+		_list.push_back(item);
+	}
+
+	void Pop()
+	{
+		_list.pop_front();
+	}
+
+	int Size()
+	{
+		return _list.size();
+	}
+
+	bool IsEmpty()
+	{
+		return Size() == 0;
+	}
+
+	T Front()
+	{
+		return _list.front();
+	}
+
+	T Back()
+	{
+		return _list.back();
+	}
+
+private:
+	list<T> _list;
+};
+
+vector<pair<int, int>> ReturnMovePoints(pair<int, int> pos, int n)
+{
+	vector<pair<int, int>> points;
+
+	for (int i = 0; i < moveCnt; i++)
+	{
+		int xPos = pos.first + movePos[i].first;
+		int yPos = pos.second + movePos[i].second;
+
+		if (xPos < 0 || xPos >= n || yPos < 0 || yPos >= n) continue;
+		points.push_back({ xPos, yPos });
+	}
+
+	return points;
+}
+
+// BFS 알고리즘을 이용한 기본적인 문제
+// 1씩 더해주면서 진행해준다.
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int cnt;
+	cin >> cnt;
+
+	vector<int> results;
+
+	for (int i = 0; i < cnt; i++)
+	{
+		int chessSize;
+		cin >> chessSize;
+
+		int** map = new int* [chessSize];
+		for (int i = 0; i < chessSize; i++)
+		{
+			map[i] = new int[chessSize] {0};
+		}
+
+		pair<int, int> start, end;
+
+		cin >> start.first >> start.second;
+		cin >> end.first >> end.second;
+
+		Queue<pair<int, int>> queue;
+		queue.Push(start);
+
+		while (queue.IsEmpty() == false)
+		{
+			pair<int, int> pos = queue.Front();
+			queue.Pop();
+
+			int value = map[pos.second][pos.first];
+			if (pos.second == end.second && pos.first == end.first)
+			{
+				results.push_back(map[end.second][end.first]);
+				break;
+			}
+
+			vector<pair<int, int>> points = ReturnMovePoints(pos, chessSize);
+			int pointSize = points.size();
+
+			for (int i = 0; i < pointSize; i++)
+			{
+				if (map[points[i].second][points[i].first] > 0) continue;
+
+				map[points[i].second][points[i].first] += value + 1;
+				queue.Push(points[i]);
+			}
+		}
+	}
+
+	int resultSize = results.size();
+	for (int i = 0; i < resultSize; i++)
+	{
+		cout << results[i];
+		if(i != resultSize - 1) cout << '\n';
+	}
+
+	return 0;
+}
