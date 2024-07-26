eadc8d963f666798ffad4faabdae05a895c5d288
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-07-26 20:51:32 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    
+    std::cout<<"SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << '\n';
+    std::cout<<"N2 Bomber      Heavy Fighter  Limited    21        " << '\n';
+    std::cout<<"J-Type 327     Light Combat   Unlimited  1         " << '\n';
+    std::cout<<"NX Cruiser     Medium Fighter Limited    18        " << '\n';
+    std::cout<<"N1 Starfighter Medium Fighter Unlimited  25        " << '\n';
+    std::cout<<"Royal Cruiser  Light Combat   Limited    4         ";
+
+    return 0;
+}
