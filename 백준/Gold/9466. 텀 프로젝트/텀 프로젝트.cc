8a6d283c5ac76d2b899f338dc71089aa78932dcf
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-07-31 13:44:04 +0900

    [Gold III] Title: 텀 프로젝트, Time: 552 ms, Memory: 4852 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -2,36 +2,26 @@
 using namespace std;
 
 int visitCount = 0;
-int loopCount = 100001;
 
 int nextVisits[100001];
 int currentIndexes[100001];
 
 bool visitAlready[100001];
 
-void dfs(int* arr, bool* visit, int nextItem, int loopIndex)
+void dfs(int* arr, int nextItem, int loopIndex)
 {
 	if (visitAlready[nextItem] == true) return; // 이미 방문해서 사이클이 형성되지 않는 것을 확인한 경우 리턴
 
 	if (nextVisits[nextItem] != 0) // 사이클을 돌아서 다시 만나는 경우
 	{
 		visitCount = loopIndex - currentIndexes[nextItem];
-		loopCount = currentIndexes[nextItem];
 		return;
 	}
 
-	if (visit[nextItem] == true) return;  // 이전에 사이클을 찾은 변수라면 리턴
-
 	nextVisits[nextItem] = arr[nextItem];
 	currentIndexes[nextItem] = loopIndex;
-	visit[nextItem] = true;
 
-	dfs(arr, visit, arr[nextItem], loopIndex + 1);
-
-	if (loopIndex < loopCount)
-	{
-		visit[nextItem] = false;
-	}
+	dfs(arr, arr[nextItem], loopIndex + 1);
 
 	visitAlready[nextItem] = true;
 	nextVisits[nextItem] = 0;
@@ -53,7 +43,6 @@ int main()
 		cin >> n;
 
 		int* arr = new int[n + 1];
-		bool* visit = new bool[n + 1];
 
 		for (int j = 1; j <= n; j++)
 		{
@@ -61,7 +50,6 @@ int main()
 			cin >> item;
 
 			arr[j] = item;
-			visit[j] = false;
 
 			nextVisits[j] = 0;
 			currentIndexes[j] = 0;
@@ -71,20 +59,16 @@ int main()
 		int resultSum = 0;
 		for (int j = 1; j <= n; j++)
 		{
-			if (visit[j] == true) continue;
-
-			dfs(arr, visit, j, 0);
+			dfs(arr, j, 0);
 
 			resultSum += visitCount;
 			visitCount = 0;
-			loopCount = 100001;
 		}
 
 		cout << n - resultSum;
 		if (i != t - 1) cout << '\n';
 
 		delete[] arr;
-		delete[] visit;
 	}
 
 	return 0;
