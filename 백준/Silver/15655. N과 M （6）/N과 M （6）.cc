cbdb49644a6cb0cc450ea2c6714d00c120bfead3
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-08-11 20:13:34 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -3,50 +3,45 @@
 using namespace std;
 
 int n, m;
+const int maxSize = 9;
+int arr[maxSize];
+int inputArr[maxSize];
 
-bool visit[100001];
-int arr[10];
-int items[10];
-
-void dfs(int cnt)
+void dfs(int count)
 {
-	if (cnt == m)
+	if (count == m)
 	{
 		for (int i = 0; i < m; i++)
 		{
 			cout << arr[i];
 			if (i != m - 1) cout << " ";
 		}
-		cout << '\n';
+
+		cout << "\n";
 	}
 	else
 	{
 		for (int i = 0; i < n; i++)
 		{
-			int item = items[i];
-			if (cnt > 0 && item <= arr[cnt - 1]) continue;
+			if (count > 0 && arr[count - 1] >= inputArr[i]) continue;
 
-			if (visit[item] == false)
-			{
-				visit[item] = true;
-				arr[cnt] = item;
-				dfs(cnt + 1);
-				visit[item] = false;
-			}
+			arr[count] = inputArr[i];
+			dfs(count + 1);
 		}
 	}
 }
+
 
 int main()
 {
 	cin >> n >> m;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> items[i];
+		cin >> inputArr[i];
 	}
 
-	sort(items, items + n);
-
+	sort(inputArr, inputArr + n);
 	dfs(0);
+
 	return 0;
 }
