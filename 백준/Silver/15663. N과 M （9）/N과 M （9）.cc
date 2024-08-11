2d9b91171077b426cad82d57173a4c7b13244caa
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-08-12 07:16:58 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+
+const int maxSize = 15;
+int arr[maxSize];
+int inputArr[maxSize];
+
+bool visit[maxSize];
+
+void dfs(int count)
+{
+	if (count == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			// 여기서 배열을 map에 넣기
+			cout << arr[i];
+			if (i != m - 1) cout << " ";
+		}
+
+		cout << '\n';
+	}
+	else
+	{
+
+		int item = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (visit[i] == true) continue;
+			if (item == inputArr[i]) continue;
+
+			visit[i] = true;
+			item = arr[count] = inputArr[i];
+			dfs(count + 1);
+			visit[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> inputArr[i];
+	}
+
+	sort(inputArr, inputArr + n);
+	dfs(0);
+
+	return 0;
+}
