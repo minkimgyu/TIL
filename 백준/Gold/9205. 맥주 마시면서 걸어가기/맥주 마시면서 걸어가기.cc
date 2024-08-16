f5577d227ce4499b2ccf6aacddad797b9e14af6b
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-08-17 00:31:02 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.cc
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.cc
@@ -1,33 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <queue>
 using namespace std;
 
-vector<pair<int, int>> ReturnClosePoints(
-	pair<int, int> pos,
-	vector<pair<int, int>> points)
-{
-	vector<pair<int, int>> targetPoints;
-
-	for (int i = 0; i < points.size(); i++)
-	{
-		pair<int, int> point = points[i];
-
-		int xDistance = abs(point.first - pos.first);
-		int yDistance = abs(point.second - pos.second);
-
-		// 20 * 50 이라서 1000m마다 보충해야함
-		// 즉 1000m 이상 이동 불가능함
-		int distance = xDistance + yDistance;
-		if (distance > 1000) continue;
-
-		targetPoints.push_back({ point.first, point.second });
-	}
-
-	return targetPoints;
-}
-
+// 범위가 1000보다 작아야 함
 
 int main()
 {
@@ -37,57 +13,61 @@ int main()
 
 	int t;
 	cin >> t;
+
 	for (int i = 0; i < t; i++)
 	{
-		int n;
-		cin >> n;
+		int pointCount;
 
-		int beerCount = 20;
-		map<pair<int, int >, bool> visit;
-
+		queue<pair<int, int>> pointQueue;
 		vector<pair<int, int>> points;
-		for (int j = 0; j < n + 2; j++)
+
+		bool visit[100];
+		for (int j = 0; j < 100; j++) visit[j] = false;
+
+		cin >> pointCount;
+
+		for (int j = 0; j < pointCount + 2; j++)
 		{
-			int start, end;
-			cin >> start >> end;
-			points.push_back({ start, end });
+			int x, y;
+			cin >> x >> y;
+
+			points.push_back({ x , y });
 		}
 
-		queue<pair<int, int>> queue;
-		queue.push(points[0]);
-		visit[points[0]] = true;
+		pointQueue.push(points[0]);
+		visit[0] = true;
 
-		bool canExit = false;
-
-		while (queue.empty() == false)
+		bool isClear = false;
+		while (pointQueue.empty() == false)
 		{
-			pair<int, int> point = queue.front();
-			queue.pop();
+			pair<int, int> point = pointQueue.front();
+			pointQueue.pop();
 
-			vector<pair<int, int>> targetPointDatas = ReturnClosePoints(point, points);
-			for (int i = 0; i < targetPointDatas.size(); i++)
+			for (int j = 0; j < pointCount + 2; j++)
 			{
-				pair<int, int> targetPoint = targetPointDatas[i];
-				if (visit[targetPoint] == true) continue;
+				if (visit[j] == true) continue;
 
-				if (targetPoint.first == points[n + 1].first && targetPoint.second == points[n + 1].second)
+				int distance = abs(point.first - points[j].first) + abs(point.second - points[j].second);
+				if (distance > 1000) continue;
+
+				if (j == pointCount + 1)
 				{
-					canExit = true;
+					cout << "happy";
+					isClear = true;
 					break;
 				}
 
-				visit[targetPoint] = true;
-				queue.push(targetPoint);
+				pointQueue.push(points[j]);
+				visit[j] = true;
 			}
 
-			if (canExit) break;
+			if (isClear == true) break;
 		}
 
-		if (canExit == true) cout << "happy";
-		else cout << "sad";
-
-		if(i != t - 1) cout << '\n';
+		if (isClear == false) cout << "sad";
+		if (i != t - 1) cout << '\n';
 	}
+	 
 
 	return 0;
 }
