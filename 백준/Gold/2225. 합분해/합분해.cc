bc04b4e2d8506632c92133f9c9939f926ef9b4e9
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-08-29 23:44:07 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -29,18 +29,39 @@ int main()
 	// k, n
 	dp[0][0] = 1; // 0부터 0까지의 정수 1개를 더해서 0으로 만드는 방법은 1개 존재함
 
-	for (int i = 1; i <= k; i++)
+	//for (int i = 1; i <= k; i++)
+	//{
+	//	for (int j = 0; j <= n; j++)
+	//	{
+	//		for (int k = 0; k <= j; k++)
+	//		{
+	//			dp[i][j] += dp[i - 1][k];
+	//			dp[i][j] %= MOD;
+	//		}
+	//	}
+	//}
+
+	// 위 식을 이용하면
+	// dp[k][n - 1] = dp[k - 1][0] +  dp[k - 1][1] + dp[k - 1][2] + dp[k - 1][3] ... + dp[k - 1][n - 1]이다
+	// 위 식에서 dp[k - 1][n]을 더해주면 dp[k][n] = dp[k][n - 1] + dp[k - 1][n]이라는 식을 얻을 수 있다.
+	// 이 방법으로도 dp식을 짤 수 있다.
+
+	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (int j = 1; j <= k; j++)
 		{
-			for (int k = 0; k <= j; k++)
-			{
-				dp[i][j] += dp[i - 1][k];
-				dp[i][j] %= MOD;
+			if (j == 1) {
+				dp[i][j] = 1;
+			}
+			else if (i == 1) {
+				dp[i][j] = j;
+			}
+			else {
+				dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
 			}
 		}
 	}
 	
-	cout << dp[k][n];
+	cout << dp[n][k];
 	return 0;
 }
