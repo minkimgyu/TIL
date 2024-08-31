1ce195296d9c2501147afd6c769dad34ec7726bc
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-08-31 23:37:27 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+const int maxSize = 100001;
+int dp[maxSize][3];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	dp[1][0] = 1;
+	dp[1][1] = 1;
+	dp[1][2] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % 9901;
+		dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+		dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+	}
+
+	cout << (dp[n][0] + dp[n][1] + dp[n][2]) % 9901;
+	return 0;
+}
