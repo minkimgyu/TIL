7dbec85ba5d64a3c6775155ba2a5607304e147e5
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-10-24 20:44:34 +0900

    [Gold V] Title: 상범 빌딩, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.cc
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.cc
@@ -1,42 +1,30 @@
 #include <iostream>
-#include <string>
 #include <queue>
-#include <vector>
+#include <string>
 using namespace std;
 
-int l, r, c;
+const int maxSize = 30;
+char arr[maxSize][maxSize][maxSize];
+int visit[maxSize][maxSize][maxSize];
 
 struct Vector3
 {
-	int x, y, z;
+public:
+	int l, r, c;
 };
 
 const int offsetSize = 6;
-Vector3 offset[offsetSize]
+Vector3 offset[offsetSize] =
 {
 	{1, 0, 0},
 	{-1, 0, 0},
+
 	{0, 1, 0},
 	{0, -1, 0},
+
 	{0, 0, 1},
 	{0, 0, -1},
 };
-
-vector<Vector3> ReturnClosePoints(Vector3 pos)
-{
-	vector<Vector3> points;
-
-	for (int i = 0; i < offsetSize; i++)
-	{
-		Vector3 point = { pos.x + offset[i].x, pos.y + offset[i].y, pos.z + offset[i].z };
-
-		if (point.x < 0 || point.x >= c || point.y < 0 || point.y >= r || point.z < 0 || point.z >= l) continue;
-
-		points.push_back(point);
-	}
-
-	return points;
-}
 
 int main()
 {
@@ -46,90 +34,87 @@ int main()
 
 	while (1)
 	{
-		int arr[30][30][30];
+		int l, r, c;
 		cin >> l >> r >> c;
+		if (l == 0 && r == 0 && c == 0) break;
 		cin.ignore();
 
-		if (l == 0 && r == 0 && c == 0) break;
-
-		queue<Vector3> queue;
+		queue<Vector3> q;
+		Vector3 endPoint;
 
 		for (int i = 0; i < l; i++)
 		{
 			for (int j = 0; j < r; j++)
 			{
-				char* line = new char[c + 1];
-				cin.getline(line, c + 1);
+				string line;
+				getline(cin, line);
 
 				for (int k = 0; k < c; k++)
 				{
-					if (line[k] == 'S')
+					char item = line[k];
+					if (item == 'S')
 					{
-						Vector3 start = { k, j, i };
-						queue.push(start);
+						q.push({ i, j, k });
+					}
+					else if (item == 'E')
+					{
+						endPoint = { i, j, k };
+					}
 
-						arr[i][j][k] = -2;
-					}
-					else if (line[k] == 'E')
-					{
-						arr[i][j][k] = -3;
-					}
-					else if (line[k] == '.')
-					{
-						arr[i][j][k] = 0;
-					}
-					else if (line[k] == '#')
-					{
-						arr[i][j][k] = -1;
-					}
+					arr[i][j][k] = item;
 				}
-
-				delete[] line;
 			}
-
 			cin.ignore();
 		}
 
-		int time = 0;
-		bool canExit = false;
+		bool nowClear = false;
 
-		while (queue.empty() == false)
+		while (q.empty() == false)
 		{
-			int queueSize = queue.size();
-			for (int i = 0; i < queueSize; i++)
+			Vector3 front = q.front();
+			int originPassedTime = visit[front.l][front.r][front.c];
+
+			q.pop();
+
+			for (int i = 0; i < offsetSize; i++)
 			{
-				Vector3 item = queue.front();
-				queue.pop();
+				Vector3 pos = { front.l + offset[i].l, front.r + offset[i].r, front.c + offset[i].c };
+				if (pos.l < 0 || pos.l >= l || pos.r < 0 || pos.r >= r || pos.c < 0 || pos.c >= c) continue;
 
-				vector<Vector3> closePoints =  ReturnClosePoints(item);
-				for (int i = 0; i < closePoints.size(); i++)
+				char item = arr[pos.l][pos.r][pos.c];
+				if (item == '#' || item == 'S') continue; // 막혔거나 시작 포인트인 경우 건너뛰기
+				if (visit[pos.l][pos.r][pos.c] > 0) continue; // 이미 방문한 지점이면 건너뛰기
+
+				if (item == 'E') // 목적지에 도달한 경우
 				{
-					Vector3 point = closePoints[i];
-					int item = arr[point.z][point.y][point.x];
-
-					if (item == -1 || item == -2) continue;
-					else if (item == -3)
-					{
-						canExit = true;
-						break;
-					}
-					else
-					{
-						arr[point.z][point.y][point.x] = -1;
-					}
-
-					queue.push(point);
+					nowClear = true;
+					cout << "Escaped in " << originPassedTime + 1 << " minute(s).\n";
+					break;
 				}
 
-				if (canExit) break;
+				visit[pos.l][pos.r][pos.c] = originPassedTime + 1;
+				q.push(pos);
 			}
 
-			time++;
-			if (canExit) break;
+			if (nowClear == true) break;
 		}
 
-		if (canExit == true) cout << "Escaped in " << time << " minute(s)." << '\n';
-		else cout << "Trapped!" << '\n';
+		if (nowClear == false)
+		{
+			cout << "Trapped!" << '\n'; // 도달하지 못한 경우
+		}
+
+		// 방문 배열 초기화
+		for (int i = 0; i < maxSize; i++)
+		{
+			for (int j = 0; j < maxSize; j++)
+			{
+				for (int k = 0; k < maxSize; k++)
+				{
+					visit[i][j][k] = 0;
+				}
+			}
+		}
 	}
 
 	return 0;
