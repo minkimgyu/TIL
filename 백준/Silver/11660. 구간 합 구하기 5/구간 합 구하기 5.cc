c8d0ccac24e4c167e0d1f4fa5aa00672988af4c5
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-10-28 16:38:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 116 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,20 +2,16 @@
 #include <vector>
 using namespace std;
 
-const int maxSize = 1025;
-int arr[maxSize][maxSize];
+int n, m;
+
+const int maxSize = 1024;
 int dp[maxSize][maxSize];
 
-const int offsetSize = 2;
-pair<int, int> offset[offsetSize] =
-{
-	{-1, 0},
-	{0, -1},
-};
+// dp에 들어갈 수 있는 최대값은 1024 * 1024 * 1000이다
+// 따라서 1,048,576,000는 int의 범위을 넘지 못하므로 int로 써도 된다.
 
-pair<int, int> crossOffset = { -1, -1 };
-
-int n, m;
+// N이 1인 경우
+//
 
 int main()
 {
@@ -24,57 +20,76 @@ int main()
 	cout.tie(NULL);
 
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 1; j <= n; j++)
+		for (int j = 0; j < n; j++)
 		{
 			int item;
-
 			cin >> item;
-			arr[i][j] = item;
+
 			dp[i][j] = item;
 		}
 	}
 
-	for (int i = 1; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 1; j <= n; j++)
+		for (int j = 0; j < n; j++)
 		{
-			for (int k = 0; k < offsetSize; k++)
-			{
-				int x = j + offset[k].first;
-				int y = i + offset[k].second;
+			int left = 0;
+			int up = 0;
+			int cross = 0;
 
-				if (x < 0 || y < 0) continue;
-				dp[i][j] += dp[y][x];
+			if (j - 1 >= 0)
+			{
+				left = dp[i][j - 1];
 			}
 
-			int x = j + crossOffset.first;
-			int y = i + crossOffset.second;
+			if (i - 1 >= 0)
+			{
+				up = dp[i - 1][j];
+			}
 
-			if (x < 0 || y < 0) continue;
-			dp[i][j] -= dp[y][x];
+			if (i - 1 >= 0 && j - 1 >= 0)
+			{
+				cross = dp[i - 1][j - 1];
+			}
+
+			dp[i][j] = dp[i][j] + left + up - cross; // cross를 겹치기 때문에 빼준다.
 		}
 	}
 
-	vector<int> results;
-
 	for (int i = 0; i < m; i++)
 	{
-		pair<int, int> start;
-		pair<int, int> end;
+		int r1, c1, r2, c2;
+		cin >> r1 >> c1 >> r2 >> c2;
 
-		cin >> start.second >> start.first >> end.second >> end.first;
-		
-		int result = dp[end.second][end.first] - dp[start.second - 1][end.first] - dp[end.second][start.first - 1] + dp[start.second - 1][start.first - 1];
-		results.push_back(result);
-	}
+		r1 -= 1;
+		c1 -= 1;
 
-	int resultSize = results.size();
-	for (int i = 0; i < resultSize; i++)
-	{
-		cout << results[i];
-		if (i != resultSize - 1) cout << '\n';
+		r2 -= 1;
+		c2 -= 1;
+
+		int left = 0;
+		int up = 0;
+		int cross = 0;
+
+		if (r1 - 1 >= 0 && c1 - 1 >= 0)
+		{
+			cross = dp[r1 - 1][c1 - 1];
+		}
+
+		if (r1 - 1 >= 0)
+		{
+			up = dp[r1 - 1][c2];
+		}
+
+		if (c1 - 1 >= 0)
+		{
+			left = dp[r2][c1 - 1];
+		}
+
+		cout << dp[r2][c2] - (left + up) + cross; // cross를 겹치기 때문에 빼준다.
+		if (i != m - 1) cout << '\n';
 	}
 
 	return 0;
