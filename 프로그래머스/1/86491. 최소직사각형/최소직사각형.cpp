99c6f642fdc3bde18829e914c275ec8020b9e36d
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-10-28 14:14:38 +0900

    [level 1] Title: 최소직사각형, Time: 0.92 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,7 +1,11 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
+
+// 명함을 돌려서 항상 가로가 더 길게 만들어준다.
 
 int solution(vector<vector<int>> sizes) {
     int answer = 0;
@@ -9,20 +13,16 @@ int solution(vector<vector<int>> sizes) {
     int maxWidth = 0;
     int maxHeight = 0;
     
-    int widthSize = sizes.size();
-    for(int i = 0; i < widthSize; i++)
+    for(int i = 0; i < sizes.size(); i++)
     {
-        if(sizes[i][0] < sizes[i][1])
-        {
-            int width = sizes[i][0];
-            sizes[i][0] = sizes[i][1];
-            sizes[i][1] = width;
-        }
+        int width = max(sizes[i][0], sizes[i][1]);
+        int height = min(sizes[i][0], sizes[i][1]);
         
-        if(maxWidth < sizes[i][0]) maxWidth = sizes[i][0];
-        if(maxHeight < sizes[i][1]) maxHeight = sizes[i][1];
+        if(maxWidth < width) maxWidth = width;
+        if(maxHeight < height) maxHeight = height;
     }
     
+    cout << maxWidth << " " << maxHeight;
     answer = maxWidth * maxHeight;
     
     return answer;
