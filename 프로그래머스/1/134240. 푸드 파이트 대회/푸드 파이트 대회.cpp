45728fe1d411571fdfb2af396ef1259c056b041b
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-10-30 17:51:59 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -6,30 +6,28 @@ using namespace std;
 string solution(vector<int> food) {
     string answer = "";
     
-    int foodSize = food.size();
+    vector<int> input;
     
+    int foodSize = food.size();
     for(int i = 1; i < foodSize; i++)
     {
-        int item = food[i];
-        int itemCount = item / 2;
-        
-        for(int j = 0; j < itemCount; j++)
+        int foodCount = food[i] / 2;
+        for(int j = 0; j < foodCount; j++)
         {
-            answer.push_back((char)(i + 48));
+            input.push_back(i);
         }
+    }
+    
+    for(int i = 0; i < input.size(); i++)
+    {
+        answer.push_back(input[i] + '0');
     }
     
     answer.push_back('0');
     
-    for(int i = foodSize - 1; i >= 1; i--)
+    for(int i = input.size() - 1; i > -1; i--)
     {
-        int item = food[i];
-        int itemCount = item / 2;
-        
-        for(int j = 0; j < itemCount; j++)
-        {
-            answer.push_back((char)(i + 48));
-        }
+        answer.push_back(input[i] + '0');
     }
     
     return answer;
