7239fc2b952d89ed6b3a99652dccb68861740fb9
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2024-10-31 18:04:35 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int n, s;
+
+const int maxSize = 100000;
+long long int arr[maxSize];
+
+//  "이 수열에서 연속된 수들의 부분합 중에 그 합이 S 이상이 되는 것 중"
+// 이 문제는 정렬하면 안 된다.
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int start = -1;
+	int end = -1;
+
+	long long int  sum = 0;
+	int minLength = maxSize;
+	int resultLength = 0;
+
+	while (start < n)
+	{
+		if (sum < s && end < n - 1)
+		{
+			resultLength++;
+			end++;
+			sum += arr[end];
+		}
+		else 
+		{
+			resultLength--;
+			start++;
+			sum -= arr[start];
+		}
+
+		if (sum >= s && minLength > resultLength)
+		{
+			minLength = resultLength;
+		}
+	}
+
+	if (minLength == maxSize)
+	{
+		cout << 0;
+	}
+	else
+	{
+		cout << minLength;
+	}
+
+	return 0;
+}
