24bb444e0bafe41305782597e765b4811e1f7778
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-02-03 10:40:24 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+const int maxSize = 100000 + 5;
+int arr[maxSize];
+int dp[maxSize];
 
 int main()
 {
@@ -11,48 +14,23 @@ int main()
 	int n, m;
 	cin >> n >> m;
 
-	int* arr = new int[n];
-	int* dp = new int[n]{0};
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
 		cin >> arr[i];
-
-		if (i > 0)
-		{
-			dp[i] = dp[i - 1] + arr[i];
-		}
-		else
-		{
-			dp[i] = arr[i];
-		}
 	}
 
-	vector<int> results;
+	for (int i = 1; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + arr[i];
+	}
 
 	for (int i = 0; i < m; i++)
 	{
-		int start, end;
-		cin >> start >> end;
+		int st, en;
+		cin >> st >> en;
 
-		int result = 0;
-
-		if (start == 1)
-		{
-			result = dp[end - 1];
-		}
-		else
-		{
-			result = dp[end - 1] - dp[start - 2];
-		}
-
-		results.push_back(result);
-	}
-
-	int resultSize = results.size();
-	for (int i = 0; i < resultSize; i++)
-	{
-		cout << results[i];
-		if (i != resultSize - 1) cout << '\n';
+		cout << dp[en] - dp[st - 1];
+		if (i != m - 1) cout << '\n';
 	}
 
 	return 0;
