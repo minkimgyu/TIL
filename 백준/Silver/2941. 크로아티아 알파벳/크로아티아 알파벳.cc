868eefa3dcd1509315af16cd0ddd0a8e5635d049
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-02-04 10:26:22 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -2,30 +2,27 @@
 #include <string>
 using namespace std;
 
-int result = 0;
-string alpha[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
-string s;
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	string s;
 	getline(cin, s);
+	int result = 0;
+
+	string rep[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
 
 	for (int i = 0; i < 8; i++)
 	{
-		int start = 0;
-		while (start < s.size() - 1 && s.find(alpha[i], start) != string::npos)
+		while (s.find(rep[i]) != string::npos)
 		{
-			start = s.find(alpha[i], start);
-			s.erase(start, alpha[i].size());
-			s.insert(start, "A");
+			int index = s.find(rep[i]);
+			s.replace(index, rep[i].size(), "R");
 		}
 	}
 
 	cout << s.size();
-
 	return 0;
 }
