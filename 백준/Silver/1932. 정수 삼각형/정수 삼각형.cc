a6225d751532053cae4c972e6b603b83474b1442
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-02-24 15:38:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -2,8 +2,9 @@
 #include <vector>
 using namespace std;
 
-int dp[200000];
-int arr[200000];
+const int maxSize = 500 + 5;
+int map[maxSize][maxSize];
+int dp[maxSize][maxSize];
 
 int main()
 {
@@ -14,48 +15,33 @@ int main()
 	int n;
 	cin >> n;
 
-	int count = 0;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i + 1; j++)
+		for (int j = 0; j <= i; j++)
 		{
-			int index = count + j;
-			int tmp;
-			cin >> tmp;
-
-			dp[index] = tmp;
-			arr[index] = tmp; // 원본 값
+			int item;
+			cin >> item;
+			map[i][j] = item;
 		}
-		count += i + 1; // 라인에 있는 숫자 개수만큼 더해줌
 	}
 
+	dp[0][0] = map[0][0];
 
-	int max = -1;
-
-	count = 0;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i + 1; j++)
+		for (int j = 0; j <= i; j++)
 		{
-			int index = count + j;
-
-			int leftChild = dp[index + i + 1];
-			int rightChild = dp[index + i + 2];
-
-			// 여기서는 원본 arr 값을 사용해서 읽어와야 한다.
-			int afterLeftChild = arr[index + i + 1] + dp[index];
-			int afterRightChild = arr[index + i + 2] + dp[index];
-
-			if (afterLeftChild > leftChild) dp[index + i + 1] = afterLeftChild;
-			if (afterRightChild > rightChild) dp[index + i + 2] = afterRightChild;
-
-			if (dp[index + i + 1] > max) max = dp[index + i + 1];
-			if (dp[index + i + 2] > max) max = dp[index + i + 2];
+			dp[i + 1][j] = max(dp[i + 1][j], map[i + 1][j] + dp[i][j]);
+			dp[i + 1][j + 1] = max(dp[i + 1][j + 1], map[i + 1][j + 1] + dp[i][j]);
 		}
-		count += i + 1; // 라인에 있는 숫자 개수만큼 더해줌
 	}
 
-	cout << max;
+	int result = 0;
+	for (int i = 0; i < n; i++)
+	{
+		result = max(result, dp[n - 1][i]);
+	}
 
+	cout << result;
 	return 0;
 }
