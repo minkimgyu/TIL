da92c9cd70b7af6c8563283976963861adcf7bb5
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-02-24 15:07:38 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -2,7 +2,8 @@
 #include <algorithm>
 using namespace std;
 
-int dp[1000001];
+const int maxSize = 1000000 + 5;
+int dp[maxSize];
 
 int main()
 {
@@ -10,34 +11,31 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	cin >> n;
+	int x;
+	cin >> x;
 
 	dp[1] = 0;
 	dp[2] = 1;
 	dp[3] = 1;
 
-	for (int i = 4; i <= n; i++)
+	for (int i = 4; i <= x; i++)
 	{
-		if (i % 2 == 0 && i % 3 == 0)
+		int minCount = maxSize;
+
+		if (i % 2 == 0)
 		{
-			dp[i] = min(min(dp[i / 3], dp[i / 2]), dp[i - 1]) + 1;
+			minCount = min(minCount, dp[i / 2] + 1);
 		}
-		else if (i % 2 == 0)
+		if (i % 3 == 0)
 		{
-			dp[i] = min(dp[i - 1], dp[i / 2]) + 1;
+			minCount = min(minCount, dp[i / 3] + 1);
 		}
-		else if (i % 3 == 0)
-		{
-			dp[i] = min(dp[i - 1], dp[i / 3]) + 1;
-		}
-		else
-		{
-			dp[i] = dp[i - 1] + 1;
-		}
+		minCount = min(minCount, dp[i - 1] + 1);
+
+		dp[i] = minCount;
 	}
 
-	cout << dp[n];
+	cout << dp[x];
 
 	return 0;
 }
