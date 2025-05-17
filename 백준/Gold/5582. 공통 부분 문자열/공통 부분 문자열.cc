ba0cccee2797b7a6ab7cdaeeec7154ed1745f2aa
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-05-18 03:21:00 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 88 ms, Memory: 64680 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int maxSize = 4000 + 5;
+int dp[maxSize][maxSize];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	int s1Size = s1.size();
+	int s2Size = s2.size();
+
+	for (int i = 0; i <= s1Size; i++)
+	{
+		for (int j = 0; j <= s2Size; j++)
+		{
+			if (i == 0 || j == 0)
+			{
+				dp[i][j] = 0;
+			}
+			else if (s1[i - 1] == s2[j - 1])
+			{
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+		}
+	}
+
+	int result = 0;
+
+	for (int i = 0; i <= s1Size; i++)
+	{
+		for (int j = 0; j <= s2Size; j++)
+		{
+			result = max(result, dp[i][j]);
+		}
+	}
+
+	cout << result;
+	return 0;
+}
