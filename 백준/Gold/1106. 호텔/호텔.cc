7b210f6112057c6ae97dc5313e389eee67e8a83f
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-05-28 20:33:09 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+const int maxPeople = 1000 + 105;
+int dp[maxPeople];
+
+pair<int, int> city[20 + 5];
+
+const int maxCost = 100 * 1000 + 5;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int c, n;
+	cin >> c >> n;
+
+	for (int i = 0; i < maxPeople; i++) dp[i] = maxCost;
+
+	for (int j = 0; j < n; j++)
+	{
+		cin >> city[j].first >> city[j].second;
+	}
+
+	dp[0] = 0;
+
+	for (int i = 0; i <= c; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i + city[j].second >= maxPeople) continue;
+			dp[i + city[j].second] = min(dp[i + city[j].second], dp[i] + city[j].first);
+		}
+	}
+
+	int result = maxCost;
+	for (int i = c; i < maxPeople; i++)
+	{
+		result = min(result, dp[i]);
+	}
+
+	cout << result;
+	return 0;
+}
