63cf3558fe478a60723fd3fc680288943dd3885b
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-05-30 00:16:59 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/15311. 약 팔기/약 팔기.cc
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -9,8 +9,8 @@ int main()
     
     cout << 1999 << '\n';
     
-    for(int i = 0; i < 1000; i++) cout << 1 << " ";
-    for(int i = 0; i < 999; i++) cout << 1000 << " ";
+    for(int i = 0; i < 999; i++) cout << 1 << " ";
+    for(int i = 0; i < 1000; i++) cout << 1000 << " ";
     
     return 0;
 }
