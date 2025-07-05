04da039761fea796ffff3ad5fbf51add3231448f
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-07-05 21:52:49 +0900

    [Gold V] Title: 카드게임, Time: 76 ms, Memory: 17868 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10835. 카드게임/카드게임.cc
+++ 백준/Gold/10835. 카드게임/카드게임.cc
@@ -11,15 +11,17 @@ int n;
 
 int dfs(int left, int right)
 {
-	if (dp[left][right] != -1) return dp[left][right]; // 이미 연산한 경우
-	if (left == n + 1 || right == n + 1) return 0; // 끝에 도달한 경우
+	if (left == n + 1 || right == n + 1) return 0; // 마지막 인덱스라면 0 반환
+	if (dp[left][right] != -1) return dp[left][right]; // 이미 연산한 경우 연산값 반환
 
-	int leftValue = dfs(left + 1, right);
-	int bothValue = dfs(left + 1, right + 1);
+	dp[left][right] = max(dp[left][right], dfs(left + 1, right));
+	dp[left][right] = max(dp[left][right], dfs(left + 1, right + 1));
+
 	int rightValue = -1;
 	if (arrL[left] > arrR[right]) rightValue = dfs(left, right + 1) + arrR[right]; // 왼쪽이 더 큰 경우 오른쪽 값 더하기
+	dp[left][right] = max(dp[left][right], rightValue);
 
-	return max(rightValue, max(leftValue, bothValue));
+	return dp[left][right];
 }
 
 int main()
