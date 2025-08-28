e679c40ac14cccea15a7ac7ef673c99347b50b9a
minkimgyu <48249824+minkimgyu@users.noreply.github.com>
2025-08-28 21:53:39 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 1012 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.cc
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.cc
@@ -1,242 +1,69 @@
 #include <iostream>
-        
-        
-          
-          #include <algorithm>
-        
-        
-          
-          using namespace std;
-        
-        
-          
-          
+#include <algorithm>
+using namespace std;
 
-        
-        
-          
-          int Hole;
-        
-        
-          
-          int N;
-        
-        
-          
-          int Lego[1000000];
-        
-        
-          
-          
+const int maxSize = 1000000 + 5;
+int arr[maxSize];
 
-        
-        
-          
-          int main() {
-        
-        
-          
-          	ios::sync_with_stdio(false);
-        
-        
-          
-          	cin.tie(); cout.tie();
-        
-        
-          
-          
+int x;
+int n;
 
-        
-        
-          
-          	while (cin >> Hole) { // 입력이 있을 때만 반복문 실행
-        
-        
-          
-          		Hole *= 10000000; // 나노미터를 기준으로 비교
-        
-        
-          
-          		cin >> N;
-        
-        
-          
-          		for (int i = 0; i < N; i++) {
-        
-        
-          
-          			cin >> Lego[i];
-        
-        
-          
-          		}
-        
-        
-          
-          		sort(Lego, Lego + N); // 레고 조각을 크기 순으로 정렬
-        
-        
-          
-          
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-        
-        
-          
-          		int left = 0, right = N - 1, sum = 0;
-        
-        
-          
-          		while (left < right) {
-        
-        
-          
-          			sum = Lego[left] + Lego[right];
-        
-        
-          
-          			if (sum < Hole) left++;
-        
-        
-          
-          			else if (sum > Hole) right--;
-        
-        
-          
-          			else break;
-        
-        
-          
-          		}
-        
-        
-          
-          
 
-        
-        
-          
-          		if (sum == Hole)
-        
-        
-          
-          			cout << "yes " << Lego[left] << " " << Lego[right] << '\n';
-        
-        
-          
-          		else
-        
-        
-          
-          			cout << "danger\n";
-        
-        
-          
-              
-        
-        
-          
-          		/*
-        
-        
-          
-          		이진 탐색 방법
-        
-        
-          
-          		int left = 0, right = 0, sum = 0;
-        
-        
-          
-          		for (int i = 0; i < N && Lego[i] <= Hole / 2; i++) {
-        
-        
-          
-          			left = i + 1, right = N - 1;
-        
-        
-          
-          			while (left <= right) {
-        
-        
-          
-          				int mid = (left + right) / 2;
-        
-        
-          
-          				sum = Lego[i] + Lego[mid];
-        
-        
-          
-          
-        
-        
-          
-          				if (sum < Hole) left = mid + 1;
-        
-        
-          
-          				else if (sum > Hole) right = mid - 1;
-        
-        
-          
-          				else {
-        
-        
-          
-          					left = i, right = mid;
-        
-        
-          
-          					i = N;
-        
-        
-          
-          					break;
-        
-        
-          
-          				}
-        
-        
-          
-          			}
-        
-        
-          
-          		}
-        
-        
-          
-          
-        
-        
-          
-          		if (sum == Hole)
-        
-        
-          
-          			cout << "yes " << Lego[left] << " " << Lego[right] << endl;
-        
-        
-          
-          		else
-        
-        
-          
-          			cout << "danger" << endl;
-        
-        
-          
-          		*/
-        
-        
-          
-          	}
-        
-        
-          
-          	return 0;
-        
-        
-          
-          }
+	while (cin >> x)
+	{
+		x *= 10000000;
+
+		cin >> n;
+
+		//for (int i = 0; i < maxSize; i++) arr[i] = 0; // 초기화
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+
+		sort(arr, arr + n);
+
+		int left = 0;
+		int right = n - 1;
+
+		bool canClear = false;
+
+		while (left < right)
+		{
+			int sum = arr[left] + arr[right];
+
+			if (sum < x)
+			{
+				left++;
+			}
+			else if (sum > x)
+			{
+				right--;
+			}
+			else
+			{
+				canClear = true;
+				break;
+			}
+		}
+
+		if (canClear == true)
+		{
+			cout << "yes " << arr[left] << " " << arr[right] << '\n';
+		}
+		else
+		{
+			cout << "danger" << '\n';
+		}
+	}
+
+
+	return 0;
+}
